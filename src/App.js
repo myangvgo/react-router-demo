01a887f1cd240ddbeb5d9cf56305ed0cffae9591
Min Yang <myangvgo@hotmail.com>
2019-11-11 23:06:34 +0800

    feat: add react Router and Link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import Users from './components/Users';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className='App'>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/users'>Users</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/users' component={Users} />
+                <Route exact path='/contact' component={Contact} />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
