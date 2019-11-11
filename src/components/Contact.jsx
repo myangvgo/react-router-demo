bf55a104393dd40df276c71b7cd74832ec5de4b1
Min Yang <myangvgo@hotmail.com>
2019-11-12 00:50:16 +0800

    feat: add programatic routing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = props => {
+    const submitForm = () => {
+        props.history.push('/');
+    };
     return (
         <div>
             <h1>Contact Page</h1>
+            <form>
+                <input placeholder='name' type='name' />
+                <input placeholder='email' type='email' />
+                <button onClick={submitForm}>Submit</button>
+            </form>
         </div>
     );
 };
